4c2088bd3d49688c7d594b900f60e39f896a13f1
Burak <buyukyukselburak@gmail.com>
2018-08-16 16:37:09 +0300

    v1.5 : changed State Priority

--- include/states.py
+++ include/states.py
@@ -5,7 +5,9 @@ from dialog import Dialog
 from subprocess import Popen,PIPE
 
 class STATES:
+	#Current State
 	current_state = 0
+	
 	def __init__(self):
 		#State : 0
 		locale.setlocale(locale.LC_ALL, '')
@@ -19,36 +21,13 @@ class STATES:
 		self.current_state += 1
 		
 	def state_01(self):
-		#State : 1
+		#State : 01
 		self.d.msgbox("Hoş Geldiniz!\n\nLütfen dosyaların taşınmasını istediğiniz bilgisayarda openssh-server uygulamasının kurulu olduğundan emin olun.",width=40,height=12)
 		self.current_state += 1
 	
 	def state_02(self):
-		#State : 2
-		code = self.d.yesno("SSH Key üretilsin mi?")
-		if(code == self.d.OK):
-			self.current_state += 1
-		#7. State'e dallan
-		else:
-			self.current_state = 7 
-	def state_03(self):
-		#SSH-KEYGEN
-		os.popen("rm -rf ~/.ssh")
-		output = Popen(['ssh-keygen', '-t', 'rsa'], stdout=PIPE, stdin=PIPE)
-		output.stdin.write("\n\n\n".encode())
-		output.stdin.close()
-		output.wait()
-		self.d.infobox("SSH KEY'ler oluşturuldu.", width=0, height=0, title="Başarılı")
-		time.sleep(2)
-		self.current_state += 1
-	
-	def state_04(self):
-		#ALERT : SSH-KEY Karşı Bilgisayar Kopyalanıcak
-		self.d.infobox("Public KEY karşı pc'ye kopyalanıcak")
-		time.sleep(1)	
-		self.current_state += 1
-	def state_05(self):
-		#Bağlanılacak PC için Bilgiler
+		#STATE 2
+		#Config Setting to Connect PC
 		isDone, tag, text = self.d.inputmenu("Bağlanılacak bilgisayarın bilgilerini giriniz",height=18, menu_height=16, choices=[("Port",self.text_port),("Host",self.text_host),("Host_Direction",self.text_host_dir),("Password",self.text_pass)])
 		if(isDone == 'renamed'):
 			if(tag == 'Port'):
@@ -63,7 +42,36 @@ class STATES:
 			self.current_state += 1
 		else:
 			self.current_state -= 1
+	
+	def state_03(self):
+		#State : 03
+		code = self.d.yesno("SSH Key üretilsin mi?")
+		if(code == self.d.OK):
+			self.current_state += 1
+		#7. State'e dallan
+		else:
+			self.current_state = 7 
+	def state_04(self):
+		#State : 04
+		#SSH-KEYGEN
+		os.popen("rm -rf ~/.ssh")
+		output = Popen(['ssh-keygen', '-t', 'rsa'], stdout=PIPE, stdin=PIPE)
+		output.stdin.write("\n\n\n".encode())
+		output.stdin.close()
+		output.wait()
+		self.d.infobox("SSH KEY'ler oluşturuldu.", width=0, height=0, title="Başarılı")
+		time.sleep(2)
+		self.current_state += 1
+	
+	def state_05(self):
+		#State : 05
+		#ALERT : SSH-KEY Karşı Bilgisayar Kopyalanıcak
+		self.d.infobox("Public KEY karşı pc'ye kopyalanıcak")
+		time.sleep(1)	
+		self.current_state += 1
+	
 	def state_06(self):
+		#State : 06
 		#SSH-COPY-ID
 		output2 = Popen(['sshpass -p "{}" ssh-copy-id -o StrictHostKeyChecking=no -p {} {} '.format(self.text_pass, self.text_port, self.text_host)], stdout=PIPE, stdin=PIPE,shell=True)
 		output2.stdin.close()
@@ -71,18 +79,24 @@ class STATES:
 		self.d.infobox("SSH KEY'ler aktarıldı.", width=0, height=0, title="Başarılı")
 		time.sleep(1)
 		self.current_state += 2
+	
 	def state_07(self):
+		#State : 07
 		#SSH-KEYGEN ÜRETİLMEDEN çalıştırma   
 		self.d.infobox("SSH KEY üretilmeden devam ediliyor.",width=40,height=3)
 		time.sleep(2)
 		self.current_state += 1
+	
 	def state_08(self):
+		#State : 08
 		isDone, self.path = self.d.dselect(self.path)
 		if isDone == 'ok':
 			self.current_state += 1
 		else:
 			self.current_state = 0
+	
 	def state_09(self):
+		#State : 09
 		onlyfiles = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path,f))]
 		onlyfiles_len = len(onlyfiles)
 		info4files = []
@@ -106,8 +120,9 @@ class STATES:
 					self.d.infobox(text = out)
 					output3.wait()
 			self.current_state += 1
-		#State : 10
+		
 	def state_repeat(self):
+		#State : 10
 		code = self.d.yesno("Dosya Yollamaya devam etmek ister misiniz?",yes_label = "Yes Baba",no_label="No Baba")
 		if(code == 'ok'):
 			self.current_state = 8
@@ -115,6 +130,7 @@ class STATES:
 			self.current_state += 1
 			
 	def state_final(self):
+		#State : 11
 		self.d.infobox("Çıkıyor Moruq")
 		time.sleep(2)
 
